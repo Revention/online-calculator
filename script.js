8a71ec8d9b922353ab05857d3eef6be0829de42d
Revention <25434575+Revention@users.noreply.github.com>
2025-03-18 17:00:37 +0100

    Added more operators to the calculate function

--- script.js
+++ script.js
@@ -7,12 +7,37 @@ const resultBox = document.getElementById('result');
 function calculate() {
   const firstNumber = firstNumberInput.value;
   const secondNumber = secondNumberInput.value;
-  //const operator = operatorInput.value;
+  const operator = operatorInput.value;
 
   // Add the numbers and show in html pane
-  let calculateResult = Number(firstNumber) + Number(secondNumber);
+  // selection based on the operator chosen
+  // DEBUG I prefer switch but first just make it work
+  if (operator == '+') {
+    let calculateResult = Number(firstNumber) + Number(secondNumber);
+    displayResult(calculateResult);
+  } else if (operator == '-') {
+    let calculateResult = Number(firstNumber) - Number(secondNumber);
+    displayResult(calculateResult);
+  } else if (operator == '*') {
+    let calculateResult = Number(firstNumber) * Number(secondNumber);
+    displayResult(calculateResult);
+  } else if (operator == '/') {
+    let calculateResult = Number(firstNumber) / Number(secondNumber);
+    displayResult(calculateResult);
+  } else if (operator == '%') {
+    let calculateResult = Number(firstNumber) % Number(secondNumber);
+    displayResult(calculateResult);
+  } else if (operator == '**') {
+    let calculateResult = Number(firstNumber) ** Number(secondNumber);
+    displayResult(calculateResult);
+  } else {
+    displayResult("not found");
+  }
+
   // document.getElementById('result').innerHTML = calculateResult;   // first solution to write result to screen
-  resultBox.textContent = 'Result: ' + calculateResult; // alternative solution slightly more elegant
+  function displayResult(calculateResult) {
+    resultBox.textContent = 'Result: ' + calculateResult; // alternative solution slightly more elegant
+  }
 }
 
 calculateButton.addEventListener('click', calculate);
