29706d8cc0e1f5466fcde597fb8536d331899f57
Revention <25434575+Revention@users.noreply.github.com>
2025-03-18 05:36:33 +0100

    Extended calculate function with adding two numbers

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const secondNumberInput = document.getElementById('secondNumber');
 const operatorInput = document.getElementById('operator');
 const calculateButton = document.getElementById('calculate');
 
-function addNumbers(firstNumber, secondNumber) {
-  return result = firstNumber + secondNumber;
-}
-
 function calculate() {
   const firstNumber = firstNumberInput.value;
   const secondNumber = secondNumberInput.value;
-  const operator = operatorInput.value;
-  let result = Number(firstNumber) + Number(secondNumber);
-  console.log(result);
+  //const operator = operatorInput.value;
+
+  // Add the numbers and show in html pane
+  let calculateResult = Number(firstNumber) + Number(secondNumber);
+  document.getElementById('result').innerHTML = calculateResult;
 }
 
 calculateButton.addEventListener('click', calculate);
