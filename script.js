7237375a816d4565c6d40efd4c824c7038f63e2a
Revention <25434575+Revention@users.noreply.github.com>
2025-03-20 11:53:49 +0100

    Removed type checking from html and added input field and division checks to javascript file

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const firstNumberInput = document.getElementById('firstNumber');
 const secondNumberInput = document.getElementById('secondNumber');
 const operatorInput = document.getElementById('operator');
 const calculateButton = document.getElementById('calculate');
-const resultBox = document.getElementById('result');
+const resultText = document.getElementById('result');      // CHANGE constant name
 
 function calculate() {
   const firstNumber = firstNumberInput.value;
   const secondNumber = secondNumberInput.value;
   const operator = operatorInput.value;
 
-  // Add the numbers and show in html pane
-  // selection based on the operator chosen
-  // DEBUG I prefer switch but first just make it work
+  // CHANGE Removed the type check from the HTML file and want to put it in the javascript file
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    resultText.textContent = 'Only numbers are allowed';
+    return;  // exit the calcuate function
+  }
+
+  // Add the numbers and show it in the html field
+  // selection based on the operator
+  // TODO I prefer switch but first just make it work
   if (operator == '+') {
     let calculateResult = Number(firstNumber) + Number(secondNumber);
     displayResult(calculateResult);
@@ -22,9 +28,17 @@ function calculate() {
     let calculateResult = Number(firstNumber) * Number(secondNumber);
     displayResult(calculateResult);
   } else if (operator == '/') {
+    if (secondNumber == 0) {
+      resultText.textContent = 'You can\'t divide by zero'      // CHANGE added division check
+      return;
+    }
     let calculateResult = Number(firstNumber) / Number(secondNumber);
     displayResult(calculateResult);
   } else if (operator == '%') {
+    if (secondNumber == 0) {
+      resultText.textContent = 'You can\'t divide by zero'      // CHANGE added division check
+      return;
+    }
     let calculateResult = Number(firstNumber) % Number(secondNumber);
     displayResult(calculateResult);
   } else if (operator == '**') {
@@ -36,7 +50,7 @@ function calculate() {
 
   // document.getElementById('result').innerHTML = calculateResult;   // first solution to write result to screen
   function displayResult(calculateResult) {
-    resultBox.textContent = 'Result: ' + calculateResult; // alternative solution slightly more elegant
+    resultText.textContent = 'Result: ' + calculateResult; // alternative solution slightly more elegant
   }
 }
 
